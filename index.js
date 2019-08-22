311b326e4c99a361516f233560e4feb2b93a14fd
Joey Romo <joeyjr95@gmail.com>
2019-08-22 15:27:45 -0400

    news app

---
+++ index.js
@@ -0,0 +1,73 @@
+'use strict';
+
+const apiKey = '4b560c6049df4e56a12bf0c7afb79d53';
+
+const searchURL = 'https://newsapi.org/v2/everything';
+
+
+function formatQueryParams(params) {
+  const queryItems = Object.keys(params)
+    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+  return queryItems.join('&');
+}
+
+function displayResults(responseJson, maxResults) {
+  // if there are previous results, remove them
+  console.log(responseJson);
+  $('#results-list').empty();
+  // iterate through the articles array, stopping at the max number of results
+  for (let i = 0; i < responseJson.articles.length & i<maxResults ; i++){
+    // for each video object in the articles
+    //array, add a list item to the results 
+    //list with the article title, source, author,
+    //description, and image
+    $('#results-list').append(
+      `<li><h3><a href="${responseJson.articles[i].url}">${responseJson.articles[i].title}</a></h3>
+      <p>${responseJson.articles[i].source.name}</p>
+      <p>By ${responseJson.articles[i].author}</p>
+      <p>${responseJson.articles[i].description}</p>
+      <img src='${responseJson.articles[i].urlToImage}'>
+      </li>`
+    )};
+  //display the results section  
+  $('#results').removeClass('hidden');
+};
+
+function getNews(query, maxResults=10) {
+  const params = {
+    q: query,
+    language: "en",
+  };
+  const queryString = formatQueryParams(params)
+  const url = searchURL + '?' + queryString;
+
+  console.log(url);
+
+  const options = {
+    headers: new Headers({
+      "X-Api-Key": apiKey})
+  };
+
+  fetch(url, options)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+    })
+    .then(responseJson => displayResults(responseJson, maxResults))
+    .catch(err => {
+      $('#js-error-message').text(`Something went wrong: ${err.message}`);
+    });
+}
+
+function watchForm() {
+  $('form').submit(event => {
+    event.preventDefault();
+    const searchTerm = $('#js-search-term').val();
+    const maxResults = $('#js-max-results').val();
+    getNews(searchTerm, maxResults);
+  });
+}
+
+$(watchForm);
